5ad513c50a6e76daea99ddb0ed72a76aa7a0a9d8
htchung <htchung@gmail.com>
2021-10-20 18:42:18 +0800

    modify database.js

--- utils/database.js
+++ utils/database.js
@@ -3,23 +3,24 @@ const { Pool } = require('pg');
 // set production variable. This will be called when deployed to a live host
 const isProduction = process.env.NODE_ENV === 'production';
 
-// if project has been deployed, connect with the host's DATABASE_URL
-// else connect with the local DATABASE_URL
-const pool = new Pool({
-  connectionString: isProduction
-    ? process.env.DATABASE_URL
-    : `postgresql://postgres:0000@localhost:5432/crown_xx`,
-});
+let pool;
 
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'crown_xx',
-//   password: '0000',
-//   port: '5432',
-// });
+if (isProduction) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'crown_xx',
+    password: '0000',
+    port: '5432',
+  });
+}
 
-pool.query('SELECT * from category', (err, res) => {
+pool.query('SELECT * from category_xx', (err, res) => {
   console.log(JSON.stringify(res.rows));
   pool.end();
 });
